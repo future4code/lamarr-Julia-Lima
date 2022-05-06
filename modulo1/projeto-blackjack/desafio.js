e6ceae55242d41f14e49ed0759be3ea8d5d3375b
Julia Marques Rodrigues de Lima <juliamarques00@hotmail.com>
2022-05-06 18:53:10 -0300

    Adicionar Projeto Blackjack

---
+++ modulo1/projeto-blackjack/desafio.js
@@ -0,0 +1,23 @@
+/* 
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ *  */
+
+    const cartaNova = comprarCarta()
+    const resultadoFinal = soma + cartaNova
+
+    while(confirm( `Suas cartas são ${carta1.texto} ${carta2.texto}. A carta revelada do computador é ${cartaPc1.texto}.
+       Deseja comprar mais uma carta?`)) {
+         cartaNova = comprarCarta()
+         console.log(resultadoFinal) 
+      
+       }
+   
+  
