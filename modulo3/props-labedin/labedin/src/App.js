a8d2b4c10b66103bb939c5d449bf63c90f047dad
Julia Marques Rodrigues de Lima <juliamarques00@hotmail.com>
2022-06-23 23:17:41 -0300

    exercicio labedin

---
+++ modulo3/props-labedin/labedin/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
+          nome="Julia Marques" 
+          descricao="Oi, eu sou Julia Marques. Estudante de Desenvolvimento Full Stack Web na Labenu, a melhor Escola de Programação."
+        />
+        
+        <ImagemButton 
+          imagem="https://cdn-icons-png.flaticon.com/512/271/271210.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Sou graduada em odontologia. Portanto, atualmente tenho 6 anos de experiência na área odontológica!" 
+        />
+        
+        <CardGrande 
+          imagem="https://logodownload.org/wp-content/uploads/2019/03/nasa-logo-0-768x768.png" 
+          nome="NASA" 
+          descricao="Tenho fluência em inglês. Em 2014 realizei uma Graduação Sanduíche de 1 ano e 6 meses na Irlanda." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
