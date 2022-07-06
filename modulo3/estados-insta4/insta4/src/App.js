81b5cbfad1d1a58df646c41e1c64f0c286c64338
Julia Marques Rodrigues de Lima <juliamarques00@hotmail.com>
2022-07-05 21:59:15 -0300

    adicionando insta4

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,36 +1,53 @@
 import React from 'react';
 import Post from './components/Post/Post';
 import './style.css'
+import {ItemLista} from './styled'
 
 
 function App() {
-return(
+  const arrayDeInformacoes = [
+    {
+     nomeUsuario: "paulinha",
+     fotoUsuario: "https://picsum.photos/50/50",
+     fotoPost: "https://picsum.photos/200/150"
+    },
+
+    {
+       nomeUsuario: 'julinha',
+       fotoUsuario: 'https://picsum.photos/id/237/200/300',
+       fotoPost: 'https://img.freepik.com/fotos-gratis/paisagem-de-nevoeiro-matinal-e-montanhas-com-baloes-de-ar-quente-ao-nascer-do-sol_335224-794.jpg?w=2000'
+
+    },
+
+    {
+       nomeUsuario: 'pedrinho',
+       fotoUsuario: 'https://www.napratica.org.br/wp-content/uploads/2019/07/michelle-obama-1.jpg',
+       fotoPost: 'https://www.grupoescolar.com/wp-content/uploads/2021/03/paisagem-2C.jpg'
+    }
+  ]
+
+  const arrayDePosts = arrayDeInformacoes.map((post, index) =>{
+    return ( 
+         <ItemLista key={index}> 
+              <Post 
+              nomeUsuario={post.nomeUsuario} 
+              fotoUsuario={post.fotoUsuario} 
+              fotoPost={post.fotoPost} 
+              />
+         </ItemLista>
+      
+    )
+  });
+ 
+return (
   <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
+    {arrayDePosts}
 
-<Post
-            nomeUsuario={'julinha'}
-            fotoUsuario={'https://picsum.photos/id/237/200/300'}
-            fotoPost={'https://img.freepik.com/fotos-gratis/paisagem-de-nevoeiro-matinal-e-montanhas-com-baloes-de-ar-quente-ao-nascer-do-sol_335224-794.jpg?w=2000'}
-          />
-
-<Post
-            nomeUsuario={'pedrinho'}
-            fotoUsuario={'https://www.napratica.org.br/wp-content/uploads/2019/07/michelle-obama-1.jpg'}
-            fotoPost={'https://www.grupoescolar.com/wp-content/uploads/2021/03/paisagem-2C.jpg'}
-          />
-    
+    {console.log(arrayDeInformacoes)}
   </div>
-  
 
-  
 )
 
 }
-
 
 export default App;
---
+++ modulo3/estados-insta4/insta4/src/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ItemLista = styled.div`
+      align-items: center;
+
+`
+
+
