0e1c8f036e4759994968e21af572fbdd138b41c6
Julia Marques Rodrigues de Lima <juliamarques00@hotmail.com>
2022-10-22 23:16:22 -0300

    alteraçoes lista

--- modulo5/lista-typescript/src/exercicio7.ts
+++ modulo5/lista-typescript/src/exercicio7.ts
@@ -1,1 +1,31 @@
 console.log("Exercício 7")
+
+const estoque = [
+	{ nome: "MacMugffin", quantidade: 37, valorUnitario: 51.040},
+	{ nome: "Vassoura voadora", quantidade: 56, valorUnitario: 210.0},
+	{ nome: "Laço da verdade", quantidade: 32, valorUnitario: 571.5},
+	{ nome: "O precioso", quantidade: 1, valorUnitario: 9181.923},
+	{ nome: "Caneta de 250 cores", quantidade: 123, valorUnitario: 17},
+	{ nome: "Plumbus", quantidade: 13, valorUnitario: 140.44},
+	{ nome: "Pokebola", quantidade: 200, valorUnitario: 99.9915}
+]
+
+type arrayEntrada = {
+    nome: string, 
+    quantidade: number,
+    valorUnitario: number
+}
+
+type arraySaida = {
+    nome: string, 
+    quantidade: number, 
+    valorUnitario: string
+}
+
+const ajustaPreco = (preco :number): string => {
+	const valorAjustado: string = preco.toFixed(2).replace('.', ',')
+	return "R$ "+ valorAjustado
+}
+
+console.log(ajustaPreco(5))
+
