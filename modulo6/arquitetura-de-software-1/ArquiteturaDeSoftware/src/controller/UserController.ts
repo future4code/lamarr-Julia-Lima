49141627ac69d7f60c3fd498d7c0f8d98bafe648
Julia Marques Rodrigues de Lima <juliamarques00@hotmail.com>
2023-01-19 22:10:45 -0300

    exercicios arquitetura de software - aula 1

---
+++ modulo6/arquitetura-de-software-1/ArquiteturaDeSoftware/src/controller/UserController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { UserBusiness } from "../business/UserBusiness";
+
+export class UserController {
+    createUser = async (req: Request, res: Response): Promise <void> => {
+        try {          
+            const input: any = {
+                name: req.body.name,
+                email: req.body.email, 
+                password: req.body.password,
+            };
+
+            const userBusiness = new UserBusiness()
+            await userBusiness.createUser(input)
+
+
+            res.status(201).send({ message: "Usuário criado!"});
+        } catch (error:any) {
+            res.status(400).send(error.message);
+            
+        }
+    }
+}
