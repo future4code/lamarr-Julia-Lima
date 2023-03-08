4c913d14e18b1080f99b48eead197c9d804a0cc4
Julia Marques Rodrigues de Lima <juliamarques00@hotmail.com>
2023-03-07 23:48:45 -0300

    testes-backend

---
+++ modulo7/testes-backend/src/controller/userRouter.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+import { UserController } from "../controller/UserController";
+
+export const userRouter = express.Router()
+
+const userController = new UserController()
+
+userRouter.get("/users/profile/:id", userController.getUserData);
+
